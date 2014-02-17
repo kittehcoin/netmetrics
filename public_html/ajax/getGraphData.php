83b4f7b8224307fe15688d35726acff989d24a28
Dan Wasyluk <dwasyluk@gmail.com>
2014-02-17 16:21:40 -0500

    Changes to charting library, fixed initial graphing, realtime updating
    - Switched out jqplot for highcharts
    - Graph now works, would like to add data range controls thus the ajax initial loading
    - Graph loads entire range of data, then live updates based on LOG_INTERVAL

--- public_html/ajax/getGraphData.php
+++ public_html/ajax/getGraphData.php
@@ -9,15 +9,37 @@ $endTime = isset($_GET['endTime']) ? $_GET['endTime'] : time();
 $db_link = mysql_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASS);
 mysql_select_db(MYSQL_DB, $db_link);
 
-$query = "SELECT * FROM diff_records WHERE timestamp > $startTime AND timestamp < $endTime";
+$query = "SELECT * FROM diff_records"; //WHERE timestamp > $startTime AND timestamp < $endTime
 $result = mysql_query($query, $db_link);
 
 if($result) {
-    $jsonResult = array();
+    $blockHeightSeries = array("name" => "Block Height",
+                               "yAxis" => 0,
+                               "color" => '#ababab',
+                               "lineWidth" => 2,
+                               "data" => array());
+
+    $diffSeries = array("name" => "Difficulty",
+                        "yAxis" => 1,
+                        "color" => '#ff0000',
+                        "data" => array());
+
+    $hashSeries = array("name" => "Hashrate",
+                        "yAxis" => 2,
+                        "color" => '#0000ff',
+                        "data" => array());
+
+    $timeSeries = array("name" => "timestamp",
+                        "data" => array());
+
     while($row = mysql_fetch_array($result)) {
-        array_push($jsonResult, array());
+        array_push($blockHeightSeries['data'], intval($row['blocknum']));
+        array_push($diffSeries['data'], floatval($row['diff']));
+        array_push($hashSeries['data'], floatval($row['nethash']));
+        array_push($timeSeries['data'], intval($row['timestamp']));
     }
 
+    $jsonResult = array($blockHeightSeries, $diffSeries, $hashSeries, $timeSeries);
     echo json_encode($jsonResult);
 }else{
     echo json_encode(array('error' => 'Database error: ' . var_dump($result)));
---
+++ public_html/ajax/getLiveGraphData.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once '../../config.php';
+
+$db_link = mysql_connect(MYSQL_SERVER, MYSQL_USER, MYSQL_PASS);
+mysql_select_db(MYSQL_DB, $db_link);
+
+$query = "SELECT * FROM diff_records ORDER BY id DESC LIMIT 1;"; //WHERE timestamp > $startTime AND timestamp < $endTime
+$result = mysql_query($query, $db_link);
+
+if($result) {
+    $blockHeightSeries = array("name" => "Block Height",
+        "data" => array());
+
+    $diffSeries = array("name" => "Difficulty",
+        "data" => array());
+
+    $hashSeries = array("name" => "Hashrate",
+        "data" => array());
+
+    $timeSeries = array("name" => "timestamp",
+        "data" => array());
+
+    while($row = mysql_fetch_array($result)) {
+        array_push($blockHeightSeries['data'], intval($row['blocknum']));
+        array_push($diffSeries['data'], floatval($row['diff']));
+        array_push($hashSeries['data'], floatval($row['nethash']));
+        array_push($timeSeries['data'], intval($row['timestamp']));
+    }
+
+    $jsonResult = array($blockHeightSeries, $diffSeries, $hashSeries, $timeSeries);
+    echo json_encode($jsonResult);
+}else{
+    echo json_encode(array('error' => 'Database error: ' . var_dump($result)));
+}
+
+
+?>
